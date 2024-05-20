8b55a5880620661d8d36e300839b8ab97b813b64
Ajay Mohan Aggarwal <ajaymohanaggarwal@gmail.com>
2024-05-20 16:36:27 +0530

    Remove bloated code.

--- src/utils.js
+++ src/utils.js
@@ -10,26 +10,6 @@ export function generateRandomNumber() {
     return arr;
 }
 
-export function getNumNotInList(arr, priorityList = []) {
-    const numList = generateRandomNumber()
-    const uniqArr = [...new Set(arr)].filter(num => num);
-    const validNumList = numList.filter(num => uniqArr.indexOf(num) === -1)
-    const validPriorityList = priorityList.filter(num => uniqArr.indexOf(num) === -1)
-
-    console.log("valid", validPriorityList)
-    if (validPriorityList.length > 0 ) {
-        return validPriorityList[0]
-    }
-
-    console.log("valid num", validNumList)
-
-    if ( validNumList.length > 0) return validNumList[0]
-    let randomIndex = getRandomNumber();
-    while(randomIndex > validNumList.length) {
-         randomIndex = getRandomNumber();
-    }
-    return validNumList[randomIndex - 1]
-}
 
 export function printSudoku(sudoku) {
     for(let arr in sudoku){
